520e98729afb4ebb679d2903dc5f2f82afa48167
Chris Partridge <chrisp@dynamicmethods.com.au>
2013-06-11 14:08:04 +0930

    Ensure full config file path is passed to config.get

--- plugins/leveldb/init_master.connection.js
+++ plugins/leveldb/init_master.connection.js
@@ -2,7 +2,7 @@ var levelup = require('levelup');
 var sublevel = require('level-sublevel');
 
 exports.hook_init_master = function(next, server) {
-  var config = this.config.get('leveldb', 'json');
+  var config = this.config.get('leveldb.json', 'json');
   var options = config.options || {};
   levelup(config.location, options, function(err, db) {
     if(err) server.logerror(err);
